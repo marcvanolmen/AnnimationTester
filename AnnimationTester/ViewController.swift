758fc9671c569b6244e6a6f8c768384453428002
Marc Van Olmen <marc.vanolmen@thomsonreuters.com>
2016-10-04 18:12:28 -0400

    added the reverse as a group animation.

--- AnnimationTester/ViewController.swift
+++ AnnimationTester/ViewController.swift
@@ -22,24 +22,39 @@ class ViewController: UIViewController {
         self.panGesture.delegate = self;
         self.view.addGestureRecognizer(self.panGesture)
     }
-
-    @IBAction func startAction(_ sender: AnyObject) {
-
-//        let animation = CABasicAnimation(keyPath:"position.y")
-//            
-//        animation.fromValue = self.greybox.frame.origin.y;
-//        animation.toValue = 0
-//        
-//        animation.duration = 5;
-//        animation.isRemovedOnCompletion = true;
-
-        let animation = CABasicAnimation(keyPath: "backgroundColor")
-        animation.fromValue = UIColor.red.cgColor
-        animation.toValue = UIColor.blue.cgColor
-        animation.duration = 5.0
+    
+    func createAnim() -> CABasicAnimation {
+        let animation = CABasicAnimation(keyPath:"position.y")
         
-        self.greybox.layer.add(animation, forKey: "Custom Animation")
-        self.greybox.layer.speed = -1
+        animation.fromValue = self.greybox.frame.origin.y;
+        animation.toValue = 0
+        animation.duration = 2;
+        return animation
+    }
+
+    
+    @IBAction func startAction(_ sender: AnyObject) {
+        // https://developer.apple.com/library/content/documentation/Cocoa/Conceptual/CoreAnimation_guide/CreatingBasicAnimations/CreatingBasicAnimations.html
+        
+        /*
+ If you want to chain two animations together so that one starts when the other finishes, do not use animation notifications. Instead, use the beginTime property of your animation objects to start each one at the desired time. To chain two animations together, set the start time of the second animation to the end time of the first animation. For more information about animation and timing values, see Customizing the Timing of an Animation.
+ 
+*/
+        let animation = self.createAnim()
+        animation.beginTime = 0
+        animation.delegate = self // This is not called
+        
+        let animation2 = self.createAnim()
+        animation2.beginTime = 2.0;
+        animation2.speed = -1;
+
+        let groupAnimation = CAAnimationGroup()
+        groupAnimation.duration = 4
+        groupAnimation.animations = [ animation, animation2]
+        groupAnimation.delegate = self // this delegate will be called.
+        
+        self.greybox.layer.add(groupAnimation, forKey: "Custom Animation")
+        self.greybox.layer.speed = 1.3
     }
     
     override func didReceiveMemoryWarning() {
@@ -47,6 +62,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 }
+
+extension ViewController: CAAnimationDelegate {
+    
+    public func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        print(")animation finsihed")
+    }
+    
+}
+
 
 extension ViewController: UIGestureRecognizerDelegate {
     
