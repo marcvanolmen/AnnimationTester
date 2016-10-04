61809f0c6b84c5e96465b3447f3fdf58e35e5562
marcvanolmen <marcvanolmen@gmail.com>
2016-10-03 22:14:36 -0400

    Adding a movable greybox to animation through gesture.

--- AnnimationTester/ViewController.swift
+++ AnnimationTester/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var panGesture: UIPanGestureRecognizer!
+    @IBOutlet weak var greybox: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    
+        self.panGesture = UIPanGestureRecognizer(target:self, action:#selector(onPanUpdate))
+        self.panGesture.delegate = self;
+        self.view.addGestureRecognizer(self.panGesture)
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+extension ViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    func onPanUpdate(_ gesture: UIPanGestureRecognizer) {
+        switch (gesture.state) {
+        case .began:
+            NSLog("begin")
+            break
+        case .changed:
+            NSLog("changed")
+            break
+        case .failed, .cancelled,.ended:
+            NSLog("end")
+            break
+        default:
+            break
+        }
+    }
 
 }
 
